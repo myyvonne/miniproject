c84de31a0f1759dbbbcb428cd466c588b82e3ac5
myyvonne <450455108@qq.com>
2019-06-03 15:53:03 +0800

    Initial commit

---
+++ src/main/java/com/thumb/mapper/UserMapper.java
@@ -0,0 +1,6 @@
+package com.thumb.mapper;
+
+
+public interface UserMapper {
+
+}
---
+++ src/main/java/com/thumb/service/UserService.java
@@ -0,0 +1,8 @@
+package com.thumb.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+}
